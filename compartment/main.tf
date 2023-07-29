9916384710487e015b94484e46c2e16012d68a18
nakao yoichi <yoichi@ad.physicist00.jp>
2023-07-29 22:04:31 +0900

    compartment更新

--- compartment/main.tf
+++ compartment/main.tf
@@ -19,7 +19,7 @@ terraform {
 resource "oci_identity_compartment" "this" {
   compartment_id = var.tenancy_ocid
   description    = "Compartment for Terraform resources."
-  name           = "${var.name_prefix}-compartment"
+  name           = var.name_prefix
 }
 
 #####################################################
